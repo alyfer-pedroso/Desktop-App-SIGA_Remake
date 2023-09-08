0769525e7b3b30fd7a273e9b6b72ed3f58747930
alyfer-pedroso <alyferleandroaraujopedroso@gmail.com>
2023-09-07 21:14:51 -0300

    first commit

---
+++ src/js/loginPerfil.js
@@ -0,0 +1,316 @@
+// Criando usuarios e seus dados
+const courses = {
+    ADM: "MTEC-PI-Administração",
+    ADM_OLD: "ETIM-Administração",
+    MODA: "MTEC-PI-Modelagem",
+};
+
+const alyfer3x4 = "../src/imgs/perfil/alyfer3x4.jpeg";
+const emily3x4 = "../src/imgs/perfil/emily3x4.jpeg";
+const erick3x4 = "../src/imgs/perfil/erick3x4.jpeg";
+const samuel3x4 = "../src/imgs/perfil/samuel3x4.jpeg";
+const pietra3x4 = "../src/imgs/perfil/pietra3x4.jpeg";
+
+// Usuarios fora do tcc
+const luis_henrique3x4 = "../src/imgs/perfil/luishenrique3x4.jpeg";
+
+class User {
+    constructor({ fullname, email, phone, rm, ra, pass, course, profileIMG }) {
+        this.fullname = fullname;
+        this.email = email;
+        this.phone = phone;
+        this.rm = rm;
+        this.ra = ra;
+        this.pass = pass;
+        this.course = course;
+        this.profileIMG = profileIMG;
+    }
+}
+
+// Usuarios do TCC
+let alyfer = new User({
+    fullname: "Alyfer Leandro de Araujo Pedroso",
+    email: "alyfer.pedroso@etec.sp.gov.br",
+    phone: "15996414866",
+    rm: "20212480114",
+    ra: "000105954137-3/SP",
+    pass: "123",
+    course: courses.ADM_OLD,
+    profileIMG: alyfer3x4,
+});
+
+let emily = new User({
+    fullname: "Emily Glinkovski",
+    email: "emily.glinkovski@etec.sp.gov.br",
+    phone: "15996259887",
+    rm: "20212480080",
+    ra: "000105072580-3/SP",
+    pass: "123",
+    course: courses.ADM_OLD,
+    profileIMG: emily3x4,
+});
+
+let erick = new User({
+    fullname: "Erick Lopez de Queiroz",
+    email: "erick.queiroz2@etec.sp.gov.br",
+    phone: "15996947314",
+    rm: "20212480044",
+    ra: "000106727048-6/SP",
+    pass: "123",
+    course: courses.ADM_OLD,
+    profileIMG: erick3x4,
+});
+
+let samuel = new User({
+    fullname: "Samuel Antonio Paes Proença",
+    email: "samuel.proenca3@etec.sp.gov.br",
+    phone: "11997717108",
+    rm: "20212480132",
+    ra: "000108565771-1/SP",
+    pass: "123",
+    course: courses.ADM_OLD,
+    profileIMG: samuel3x4,
+});
+
+let pietra = new User({
+    fullname: "Pietra Rosa Baião",
+    email: "pietra.baiao@etec.sp.gov.br",
+    phone: "15991317691",
+    rm: "20212480069",
+    ra: "000105960617-3/SP",
+    pass: "123",
+    course: courses.ADM_OLD,
+    profileIMG: pietra3x4,
+});
+
+// Usuarios fora do TCC
+let luis_henrique = new User({
+    fullname: "Luis Henrique Santos de Jesus",
+    email: "luis.jesus18@etec.sp.gov.br",
+    phone: "15988070641",
+    rm: "20212480140",
+    ra: "000106996518-2/SP",
+    pass: "157155",
+    course: courses.ADM_OLD,
+    profileIMG: luis_henrique3x4,
+});
+
+//Verificando para login
+const $loginDisplay = document.getElementById("loginBody");
+const $alunoDisplay = document.getElementById("alunoBody");
+
+if ($loginDisplay) {
+    const $form = document.getElementById("loginForm");
+    let $checkRemember = document.getElementById("remember");
+    let $user = document.getElementById("user");
+    let $pass = document.getElementById("pass");
+
+    if (localStorage.remember == "true") {
+        open("../src/aluno.html", "_self");
+    }
+
+    function areUser(e) {
+        e.preventDefault();
+        switch ($user.value) {
+            // Usuarios do TCC
+            case alyfer.rm: {
+                if ($pass.value === alyfer.pass) {
+                    if (localStorage.user) {
+                        localStorage.clear();
+                    }
+                    localStorage.setItem("User", "alyfer");
+                    if ($checkRemember.checked == true) {
+                        localStorage.setItem("remember", "true");
+                    } else {
+                        localStorage.setItem("remember", "false");
+                    }
+                    open("../src/aluno.html", "_self");
+                } else {
+                    alert("Senha incorreta!");
+                    $pass.value = "";
+                }
+                break;
+            }
+
+            case emily.rm: {
+                if ($pass.value === emily.pass) {
+                    if (localStorage.user) {
+                        localStorage.clear();
+                    }
+                    localStorage.setItem("User", "emily");
+                    if ($checkRemember.checked == true) {
+                        localStorage.setItem("remember", "true");
+                    } else {
+                        localStorage.setItem("remember", "false");
+                    }
+                    open("../src/aluno.html", "_self");
+                } else {
+                    alert("Senha incorreta!");
+                    $pass.value = "";
+                }
+                break;
+            }
+
+            case erick.rm: {
+                if ($pass.value === erick.pass) {
+                    if (localStorage.user) {
+                        localStorage.clear();
+                    }
+                    localStorage.setItem("User", "erick");
+                    if ($checkRemember.checked == true) {
+                        localStorage.setItem("remember", "true");
+                    } else {
+                        localStorage.setItem("remember", "false");
+                    }
+                    open("../src/aluno.html", "_self");
+                } else {
+                    alert("Senha incorreta!");
+                    $pass.value = "";
+                }
+                break;
+            }
+
+            case samuel.rm: {
+                if ($pass.value === samuel.pass) {
+                    if (localStorage.user) {
+                        localStorage.clear();
+                    }
+                    localStorage.setItem("User", "samuel");
+                    if ($checkRemember.checked == true) {
+                        localStorage.setItem("remember", "true");
+                    } else {
+                        localStorage.setItem("remember", "false");
+                    }
+                    open("../src/aluno.html", "_self");
+                } else {
+                    alert("Senha incorreta!");
+                    $pass.value = "";
+                }
+                break;
+            }
+
+            case pietra.rm: {
+                if ($pass.value === pietra.pass) {
+                    if (localStorage.user) {
+                        localStorage.clear();
+                    }
+                    localStorage.setItem("User", "pietra");
+                    if ($checkRemember.checked == true) {
+                        localStorage.setItem("remember", "true");
+                    } else {
+                        localStorage.setItem("remember", "false");
+                    }
+                    open("../src/aluno.html", "_self");
+                } else {
+                    alert("Senha incorreta!");
+                    $pass.value = "";
+                }
+                break;
+            }
+
+            // Usuarios fora do TCC
+            case luis_henrique.rm: {
+                if ($pass.value === luis_henrique.pass) {
+                    if (localStorage.user) {
+                        localStorage.clear();
+                    }
+                    localStorage.setItem("User", "luis_henrique");
+                    if ($checkRemember.checked == true) {
+                        localStorage.setItem("remember", "true");
+                    } else {
+                        localStorage.setItem("remember", "false");
+                    }
+                    open("../src/aluno.html", "_self");
+                } else {
+                    alert("Senha incorreta!");
+                    $pass.value = "";
+                }
+                break;
+            }
+
+            default: {
+                alert("Usuario não encontrado!");
+                $user.value = "";
+                $pass.value = "";
+                break;
+            }
+        }
+    }
+
+    $form.addEventListener("submit", areUser);
+}
+
+// Verificando qual usuário foi logado
+const whichUser = setInterval(() => {
+    if ($alunoDisplay) {
+        let $perfilIMG = document.querySelector(".dados__img img");
+        let $perfilNAME = document.querySelector(".dados__p #fullname");
+        let $perfilRM = document.querySelector(".dados__p #rm");
+        let $perfilRA = document.querySelector(".dados__p #ra");
+
+        // Usuarios do TCC
+        switch (localStorage.getItem("User")) {
+            case "alyfer": {
+                $perfilIMG.src = alyfer.profileIMG;
+                $perfilNAME.innerHTML = alyfer.fullname;
+                $perfilRM.innerHTML = alyfer.rm;
+                $perfilRA.innerHTML = alyfer.ra;
+                clearInterval(whichUser);
+                break;
+            }
+
+            case "emily": {
+                $perfilIMG.src = emily.profileIMG;
+                $perfilNAME.innerHTML = emily.fullname;
+                $perfilRM.innerHTML = emily.rm;
+                $perfilRA.innerHTML = emily.ra;
+                clearInterval(whichUser);
+                break;
+            }
+
+            case "erick": {
+                $perfilIMG.src = erick.profileIMG;
+                $perfilNAME.innerHTML = erick.fullname;
+                $perfilRM.innerHTML = erick.rm;
+                $perfilRA.innerHTML = erick.ra;
+                clearInterval(whichUser);
+                break;
+            }
+
+            case "samuel": {
+                $perfilIMG.src = samuel.profileIMG;
+                $perfilNAME.innerHTML = samuel.fullname;
+                $perfilRM.innerHTML = samuel.rm;
+                $perfilRA.innerHTML = samuel.ra;
+                clearInterval(whichUser);
+                break;
+            }
+
+            case "pietra": {
+                $perfilIMG.src = pietra.profileIMG;
+                $perfilNAME.innerHTML = pietra.fullname;
+                $perfilRM.innerHTML = pietra.rm;
+                $perfilRA.innerHTML = pietra.ra;
+                clearInterval(whichUser);
+                break;
+            }
+
+            // Usuarios do fora do TCC
+            case "luis_henrique": {
+                $perfilIMG.src = luis_henrique.profileIMG;
+                $perfilNAME.innerHTML = luis_henrique.fullname;
+                $perfilRM.innerHTML = luis_henrique.rm;
+                $perfilRA.innerHTML = luis_henrique.ra;
+                clearInterval(whichUser);
+                break;
+            }
+
+            default: {
+                localStorage.clear();
+                open("../index.html", "_self");
+            }
+        }
+    } else {
+        return;
+    }
+});
---
+++ src/js/main.js
@@ -0,0 +1,186 @@
+(() => {
+    // DISPLAY
+    const body = document.querySelector("body");
+    const $mainAluno = document.querySelector(".mainAluno");
+    const $mainAluno__displays = document.querySelectorAll(".display");
+
+    // BTNS
+    const footerButtons = document.querySelectorAll(".footerAluno button");
+    const $hamburguerMenu = document.querySelector(".aluno-hamburguerMenu");
+    const exitButton = document.getElementById("exit");
+    const allOptBtn = document.querySelector(".optionsNav__btn");
+
+    // OPT BTNS
+    const $btn__vidaAcad = document.querySelectorAll(".OPT-VIDAACAD");
+    const $btn__boletimES = document.querySelectorAll(".OPT-BOESC");
+    const $btn__fichaInd = document.querySelectorAll(".OPT-FICHAIND");
+    const $btn__fichaDes = document.querySelectorAll(".OPT-FICHADES");
+    const $btn__progPar = document.querySelectorAll(".OPT-PROGPAR");
+    const $btn__horarioCur = document.querySelectorAll(".OPT-HRCUR");
+    const $btn__solicitDoc = document.querySelectorAll(".OPT-SOLIDOC");
+    const $btn__aprovEs = document.querySelectorAll(".OPT-APROV");
+    const $btn__emailIns = document.querySelectorAll(".OPT-EMAILINS");
+    const $btn__rematric = document.querySelectorAll(".OPT-REMA");
+    const $btn__comprovVac = document.querySelectorAll(".OPT-COMVAC");
+    const $btn__reconsid = document.querySelectorAll(".OPT-RECO");
+    const $btn__reclass = document.querySelectorAll(".OPT-RECLA");
+    const $btn__attCad = document.querySelectorAll(".OPT-ATTC");
+
+    let isNotMenu = false;
+    let isHome = true;
+
+    // Page pre-load
+    window.addEventListener("load", () => {
+        const loader = document.querySelector(".loading-loader");
+        loader.classList.add("lAluno-hidden");
+        loader.addEventListener("transitionend", () => {
+            loader.remove();
+        });
+    });
+
+    if (isHome) {
+        footerButtons.forEach((el) => {
+            el.classList.remove("markedBTN");
+        });
+        $mainAluno__displays.forEach((el) => {
+            el.style.display = "none";
+        });
+        $mainAluno__displays[0].style.display = "block";
+        footerButtons[0].classList.add("markedBTN");
+    }
+
+    function loaderPage() {
+        loader = document.createElement("div");
+        loader.classList = "loading-loader lAluno";
+        body.appendChild(loader);
+        setTimeout(() => {
+            loader.classList.add("lAluno-hidden");
+        }, 75);
+        loader.addEventListener("transitionend", () => {
+            loader.remove();
+        });
+    }
+
+    // Funções das páginas
+    function initPage() {
+        scrollTo(0, 0);
+        location.reload();
+    }
+
+    function goToPage() {
+        $mainAluno__displays.forEach((el) => {
+            el.style.display = "none";
+        });
+        footerButtons.forEach((el) => {
+            el.classList.remove("markedBTN");
+        });
+        $hamburguerMenu.classList.remove("opened");
+        isNotMenu = true;
+        isHome = false;
+        loaderPage();
+        scrollTo(0, 0);
+    }
+
+    function accessAllOptions() {
+        setTimeout(() => {
+            goToPage();
+            $mainAluno__displays[1].style.display = "block";
+        }, 10);
+    }
+
+    function accessVidaAcad() {
+        setTimeout(() => {
+            goToPage();
+            $mainAluno__displays[2].style.display = "block";
+        }, 10);
+    }
+
+    function accessBoEsc() {
+        setTimeout(() => {
+            goToPage();
+            $mainAluno__displays[3].style.display = "block";
+        }, 10);
+    }
+
+    function accessFichaInd() {
+        setTimeout(() => {
+            goToPage();
+            $mainAluno__displays[4].style.display = "block";
+        }, 10);
+    }
+
+    function accessFichaDes() {
+        setTimeout(() => {
+            goToPage();
+            $mainAluno__displays[5].style.display = "block";
+        }, 10);
+    }
+
+    function accessProgPar() {
+        setTimeout(() => {
+            goToPage();
+            $mainAluno__displays[6].style.display = "block";
+        }, 10);
+    }
+
+    // Adciocionando eventos nos botoes
+    allOptBtn.addEventListener("click", accessAllOptions);
+    $btn__vidaAcad.forEach((el) => {
+        el.addEventListener("click", accessVidaAcad);
+    });
+    $btn__boletimES.forEach((el) => {
+        el.addEventListener("click", accessBoEsc);
+    });
+    $btn__fichaInd.forEach((el) => {
+        el.addEventListener("click", accessFichaInd);
+    });
+    $btn__fichaDes.forEach((el) => {
+        el.addEventListener("click", accessFichaDes);
+    });
+    $btn__progPar.forEach((el) => {
+        el.addEventListener("click", accessProgPar);
+    });
+
+    footerButtons[0].addEventListener("click", () => {
+        if (isNotMenu) {
+            initPage();
+            scrollTo(0, 0);
+        } else {
+            if ($hamburguerMenu.classList == "aluno-hamburguerMenu opened") {
+                footerButtons.forEach((el) => {
+                    el.classList.remove("markedBTN");
+                });
+                body.style.overflow = "visible";
+            }
+            $hamburguerMenu.classList.remove("opened");
+            footerButtons[0].classList.add("markedBTN");
+        }
+    });
+
+    footerButtons[1].addEventListener("click", () => {
+        $hamburguerMenu.classList.toggle("opened");
+        if ($hamburguerMenu.classList == "aluno-hamburguerMenu opened") {
+            footerButtons.forEach((el) => {
+                el.classList.remove("markedBTN");
+            });
+            footerButtons[1].classList.add("markedBTN");
+            body.style.overflow = "hidden";
+        } else {
+            footerButtons.forEach((el) => {
+                el.classList.remove("markedBTN");
+            });
+            if (isHome) {
+                footerButtons.forEach((el) => {
+                    el.classList.remove("markedBTN");
+                });
+                footerButtons[0].classList.add("markedBTN");
+            }
+            body.style.overflow = "visible";
+        }
+    });
+
+    exitButton.addEventListener("click", () => {
+        localStorage.clear();
+        open("../src/index.html", "_self");
+    });
+})();
